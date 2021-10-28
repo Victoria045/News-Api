fa4ecd7a93f2dbb113eda1cd80fef6b6ece7a751
Victoria045 <victoriaberyl45@gmail.com>
2021-10-28 13:42:51 +0300

    create the article model class

--- app/models.py
+++ app/models.py
@@ -6,3 +6,15 @@ class Source:
         self.id = id
         self.name = name
         self.desc = description
+
+class Article:
+    '''
+    Article class to define Article Objects
+    '''
+    def __init__(self, title, author, image_url, date_published, article_url, source):
+        self.title = title
+        self.author = author
+        self.date_published = date_published
+        self.article_url = article_url
+        self.image_url = image_url
+        self.source = source
