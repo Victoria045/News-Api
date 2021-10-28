f046d580bb15725645e7e1bdce56448f842e8ad3
Victoria045 <victoriaberyl45@gmail.com>
2021-10-28 14:19:26 +0300

    create a route decorator and define the view fumction

--- app/main/views.py
+++ app/main/views.py
@@ -1,2 +1,8 @@
 from flask import render_template
 from . import main
+
+@main.route('/')
+def index():
+    sources_list = get_sources()
+    title = 'Sources'
+    return render_template('index.html', sources = sources_list, title= title)
