8f9fc6ce4959df2ed9ece1b5ac40d6dfcd7e4a3f
Victoria045 <victoriaberyl45@gmail.com>
2021-10-28 13:37:21 +0300

    import the configure_request() function from the request.py file

--- app/__init__.py
+++ app/__init__.py
@@ -14,9 +14,15 @@ def create_app(config_name):
 
     #initializing flask extensions
     bootstrap.init_app(app)
+    # moment.init_app(app)
 
     # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    # setting config 
+
+    from .requests import configure_request
+    configure_request(app)
 
     return app
