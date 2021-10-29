db8416e725ee82d2ffcd68b1a6a49181872a6977
Victoria045 <victoriaberyl45@gmail.com>
2021-10-29 06:49:33 +0300

    define various routes in the application

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,23 @@
 from flask import render_template
 from . import main
+from ..requests import get_sources, get_specific_source
 
 @main.route('/')
 def index():
+    '''
+    View root page function that returns the index page and its data
+    '''
     sources_list = get_sources()
-    title = 'Sources'
+    title = 'News Sources'
     return render_template('index.html', sources = sources_list, title= title)
+
+
+@main.route('/source/<name>')
+def source(name):
+    articles_list = get_specific_source(name)
+    if len(articles_list) > 0:
+        source = articles_list[0].source['name']
+    else:
+        source = None
+    title = f"{name} articles"
+    return render_template('articles.html', articles = articles_list, title=title, source = source)
